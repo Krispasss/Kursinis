aeeb9cbddae8720a5a6894da0fe5b6f239430719
Krispasss <159714010+Krispasss@users.noreply.github.com>
2024-04-22 21:55:21 +0300

    Encapsulation, bug fix
    Added encapsulation, getters.
    Fixed restoring deleted content in a file, copy method.

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import os
 import shutil
 
+
 class Action:
     def __init__(self):
-        self.actions = []
-        self.removed_file_content = []
-        self.source_directory = []
-        self.destination_directory = []
-        self.last_file_name = []
+        self.__actions = []
+        self.__removed_file_content = []
+        self.__source_directory = []
+        self.__destination_directory = []
+        self.__last_file_name = []
 
+
+    def get_actions(self):
+        return self.__actions
+    def get_removed_file_name(self):
+        return self.__last_file_name
+    def get_source_directory(self):
+        return self.__source_directory
+    def get_destination_directory(self):
+        return  self.__destination_directory
+    def get_removed_file_content(self):
+        return self.__removed_file_content
     def add_action(self, action):
-        self.actions.append(action)
+        self.__actions.append(action)
 
     def show_actions(self):
-        if self.actions:
-            print("Last action:", self.actions[-1])
+        if self.__actions:
+            print("Last action:", self.__actions[-1])
         else:
             print("No actions performed yet.")
 
@@ -51,26 +63,29 @@ class Action:
     def save_file_contents_to_list(self, file_name):
         file_path = os.path.join(os.getcwd(), file_name)
         if os.path.exists(file_path):
-            with open(file_path, 'r') as file:
-                self.removed_file_content.append(file.readlines())
+
+            with open(file_name) as file:
+                for line in file:
+                    line = line.strip()  # or some other preprocessing
+
+                    self.__removed_file_content.append(line)
+
             print(f"File '{file_name}' contents have been saved to a list.")
         else:
             print(f"File '{file_name}' does not exist in the current directory.")
 
     def copy_file(self, source, destination):
         if os.path.exists(source):
-            shutil.move(source, destination)
-            print(f"Failas {source} perkeltas į {destination}.")
+            shutil.copy(source, destination)
+            print(f"File {source} was transfered to {destination}.")
             self.add_action({'type': 'move_file', 'source': source, 'destination': destination})
         else:
-            print(f"Klaida: Failas {source} neegzistuoja.")
+            print(f"Error: File {source} does not exist.")
 
     def move_file(self, source, destination):
-        self.last_file_name = source
-
-
-        self.source_directory = os.getcwd()
-        self.destination_directory = destination
+        self.__last_file_name = source
+        self.__source_directory = os.getcwd()
+        self.__destination_directory = destination
         shutil.move(source, destination)
         action.add_action({'type': 'move_file', 'source': source, 'destination': destination})
         print("File moved successfully.")
@@ -81,21 +96,23 @@ class Action:
         print("Directory created successfully.")
 
 class Restore(Action):
-    def __init__(self, actions, file_name, file_source, file_destination):
+    def __init__(self, __actions, file_name, file_source, file_destination, removed_file_content):
         super().__init__()
-        self.history = actions
-        self.source_directory = file_source
-        self.destination_directory = file_destination
-        self.last_file_name = file_name
+        self.history = __actions
+        self.__source_directory = file_source
+        self.__destination_directory = file_destination
+        self.__last_file_name = file_name
+        self.__removed_file_content = removed_file_content
 
 
     def undo_last_action(self):
+
         if self.history:
             last_action = (self.history.pop())
             if last_action['type'] == 'create_file':
                 self.remove_file(last_action['filename'])
             elif last_action['type'] == 'remove_file':
-                self.add_file(last_action['filename'])
+                self.restore_file(last_action['filename'])
             elif last_action['type'] == 'create_directory':
                 os.rmdir(last_action['directory'])
             elif last_action['type'] == 'remove_directory':
@@ -106,16 +123,18 @@ class Restore(Action):
                 self.move_file("source", "dsa")
 
     def move_file(self, source, destination):
-
-        source = os.path.join(os.getcwd(), self.destination_directory, self.last_file_name)
-
-
-        self.source_directory = os.getcwd()
-
-        shutil.move(source, self.source_directory)
+        source = os.path.join(os.getcwd(), self.__destination_directory, self.__last_file_name)
+        self.__source_directory = os.getcwd()
+        shutil.move(source, self.__source_directory)
         action.add_action({'type': 'move_file', 'source': source, 'destination': destination})
         print("File moved successfully.")
 
+    def restore_file (self, file_name):
+
+        file = open(file_name, 'w')
+        for line in self.__removed_file_content:
+            file.write(line + "\n")
+        file.close()
 
 
 
@@ -137,15 +156,15 @@ def main():
         choice = input("Enter your choice (1-9): ")
 
         if choice == '1':
-            file_path = input("Enter the path of the file to add: ")
+            file_path = input("Enter the file name to add: ")
             action.add_file(file_path)
         elif choice == '2':
-            file_path = input("Enter the path of the file to remove: ")
+            file_path = input("Enter the file name to remove: ")
             action.remove_file(file_path)
         elif choice == '3':
             action.show_files()
         elif choice == '4':
-            source = input("Enter the source file path: ")
+            source = input("Enter the file name: ")
             destination = input("Enter the destination file path: ")
             action.copy_file(source, destination)
         elif choice == '5':
@@ -158,8 +177,9 @@ def main():
         elif choice == '7':
             action.show_actions()
         elif choice == '8':
-            restore = Restore(action.actions, action.last_file_name, action.source_directory, action.destination_directory)
+            restore = Restore(action.get_actions(), action.get_removed_file_name(), action.get_source_directory(), action.get_destination_directory(), action.get_removed_file_content())
             restore.undo_last_action()
+            print("Restored last action.")
         elif choice == '9':
             print("Exiting program.")
             break
